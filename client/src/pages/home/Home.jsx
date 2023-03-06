7e72d89820af4364ed7ec68ec322c22ad9cd1ea7
trongtuan99er <trongtuan681@gmail.com>
2023-03-06 16:46:00 +0700

    cosmetic

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./home.scss";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import List from "../../components/list/List.jsx";
+import Featured from "../../components/featured/Featured.jsx"
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <Navbar />
+      <Featured />
+      <List/>
+      <List/>
+      <List/>
+      <List/>
+    </div>
+  );
+};
 
 export default Home
