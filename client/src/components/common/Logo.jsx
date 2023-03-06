0606ac38256507a45aaaebb7bab3ddcc9bcd8bf7
trongtuan99er <trongtuan681@gmail.com>
2023-03-06 16:28:43 +0700

    Styled login page

---
+++ client/src/components/common/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <h2 className="logo" style={{fontWeight: '700', fontSize: '1.5rem', display: 'inline-block'}}>
+      <span style={{color: '#ff0000'}}>Notflix</span>
+    </h2>
+  );
+};
+
+export default Logo;
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Logo from "../../components/common/Logo";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <Logo />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Đăng nhập</h1>
+          <input type="email" placeholder="Email hoặc SDT" />
+          <input type="password" placeholder="Mật khẩu" />
+          <button className="loginButton">Đăng nhập</button>
+          <span>
+            Chưa có tài khoản Notflix ? <a href="/register"><b>Đăng ký ngay !</b></a>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login
